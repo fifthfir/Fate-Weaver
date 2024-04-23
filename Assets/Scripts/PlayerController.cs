8107cd6b06060173c2236d31c0454642367950ba
fifthfir <fifthfir2017@gmail.com>
2024-04-22 17:27:34 -0700

    Add Avatar change in dialog

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed;
+    public static PlayerController instance;
+
+	public float moveSpeed;
 
 	private Animator anim;
 	private SpriteRenderer theSR;
-	public Rigidbody2D theRB;
+	private Rigidbody2D theRB;
+	public bool stopInput;
+
+	private void Awake()
+	{
+		instance = this;
+	}
 
 	// Start is called before the first frame update
     void Start()
@@ -16,50 +24,56 @@ public class PlayerController : MonoBehaviour
 		anim = GetComponent<Animator>();
 		theSR = GetComponent<SpriteRenderer>();
 		theRB = GetComponent<Rigidbody2D>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
-		float horizontalInput = Input.GetAxisRaw("Horizontal");
-        float verticalInput = Input.GetAxisRaw("Vertical");
+		if (!stopInput) {
+			float horizontalInput = Input.GetAxisRaw("Horizontal");
+			float verticalInput = Input.GetAxisRaw("Vertical");
 
-		bool moveHorizontal = true;
-		bool moveUp = false;
-		bool moveDown = false;
+			bool moveHorizontal = true;
+			bool moveUp = false;
+			bool moveDown = false;
 
-		if (horizontalInput < 0) {
-			theSR.flipX = false;
-		} else if (horizontalInput > 0) {
-			theSR.flipX = true;
+			if (horizontalInput < 0) {
+				theSR.flipX = false;
+			} else if (horizontalInput > 0) {
+				theSR.flipX = true;
+			} else {
+				moveHorizontal = false;
+			}
+
+			if (verticalInput > 0) {
+				moveUp = true;
+				moveDown = false;
+			} else if (verticalInput < 0) {
+				moveUp = false;
+				moveDown = true;
+			} else {
+				moveUp = false;
+				moveDown = false;
+			}
+
+			// Vector3 moveDirection = new Vector3(horizontalInput, verticalInput, 0f);
+			// transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+			theRB.velocity = new Vector2(moveSpeed * horizontalInput, moveSpeed * verticalInput);
+			// No squash, good!
+
+			// anim.SetFloat("moveSpeed", horizontalInput * horizontalInput + verticalInput * verticalInput);
+			anim.SetBool("moveHorizontal", moveHorizontal);
+			anim.SetBool("moveUp", moveUp);
+			anim.SetBool("moveDown", moveDown);
+
+			theRB.freezeRotation = true;
 		} else {
-			moveHorizontal = false;
+			theRB.velocity = new Vector2(0, 0);
+			anim.SetBool("moveHorizontal", false);
+			anim.SetBool("moveUp", false);
+			anim.SetBool("moveDown", false);
 		}
-
-		if (verticalInput > 0) {
-			moveUp = true;
-		    moveDown = false;
-		} else if (verticalInput < 0) {
-			moveUp = false;
-		    moveDown = true;
-		} else {
-			moveUp = false;
-		    moveDown = false;
-		}
-
-		// Vector3 moveDirection = new Vector3(horizontalInput, verticalInput, 0f);
-		// transform.position += moveDirection * moveSpeed * Time.deltaTime;
-
-		theRB.velocity = new Vector2(moveSpeed * horizontalInput, moveSpeed * verticalInput);
-		// No squash, good!
-
-		// anim.SetFloat("moveSpeed", horizontalInput * horizontalInput + verticalInput * verticalInput);
-		anim.SetBool("moveHorizontal", moveHorizontal);
-		anim.SetBool("moveUp", moveUp);
-		anim.SetBool("moveDown", moveDown);
-
-		theRB.freezeRotation = true;
 
     }
 }
